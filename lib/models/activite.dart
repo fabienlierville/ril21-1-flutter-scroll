512db29b9eeaf322c99a3e2ff9b53c00efd438bf
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-28 12:05:23 +0100

    ListView

---
+++ lib/models/activite.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Activie{
+  String nom;
+  IconData icone;
+
+  Activie({required this.nom, required this.icone});
+}
--- lib/pages/page_list_view.dart
+++ lib/pages/page_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scroll/models/activite.dart';
 
 class PageListView extends StatefulWidget {
   const PageListView({Key? key}) : super(key: key);
@@ -8,6 +9,44 @@ class PageListView extends StatefulWidget {
 }
 
 class _PageListViewState extends State<PageListView> {
+  List<Activie> activites = [
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+    Activie(nom: "Vélo", icone: Icons.directions_bike),
+    Activie(nom: "Peinture", icone: Icons.palette),
+    Activie(nom: "Golf", icone: Icons.golf_course),
+    Activie(nom: "Jeux", icone: Icons.gamepad),
+    Activie(nom: "Bricolage", icone: Icons.build),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +54,24 @@ class _PageListViewState extends State<PageListView> {
         title: Text("List View"),
       ),
       body: Center(
-
+        child: ListView.builder(
+          itemCount: activites.length,
+          itemBuilder: (context, index){
+            Activie activite = activites[index];
+            return ListTile(
+              title: Text("Activite:"),
+              subtitle: Text(activite.nom),
+              trailing: IconButton(
+                onPressed: null,
+                icon: Icon(Icons.edit),
+              ),
+              leading: Icon(activite.icone),
+              onTap: (){
+                print(activite.nom);
+              },
+            );
+          },
+        ),
       ),
     );
   }
